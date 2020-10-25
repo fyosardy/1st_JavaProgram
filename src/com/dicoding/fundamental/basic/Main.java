1db0f2a124b88e592a1db5a2caee592b9e4f6b23
Febrian Yosardy <fyosardy@gmail.com>
2020-10-25 21:06:52 +0700

    Add Pakage Gitar

--- src/com/dicoding/fundamental/basic/Main.java
+++ src/com/dicoding/fundamental/basic/Main.java
@@ -1,7 +1,11 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.fundamental.basic.musik.Gitar;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Gitar.bunyi();
     }
 }
